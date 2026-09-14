55ad06ef7d80a88bbcba053a2ea44387ce0c0e04
Vlad Popovici <vlad.popovici@recetox.muni.cz>
2026-09-14 20:50:12 +0200

    e1011 initial

--- config_public_terminal.js
+++ config_public_terminal.js
@@ -1,7 +1,11 @@
 // The root OS image location, change to local filepath if serving locally
-export const diskImageUrl = "wss://disks.webvm.io/debian_buster_large_permis_fixed_01-06-2026.ext2";
+// original:
+// export const diskImageUrl = "wss://disks.webvm.io/debian_buster_large_permis_fixed_01-06-2026.ext2";
+export const diskImageUrl = "/custom-disk-images/e1011_image.ext2";
 // The root filesystem backend type use "cloud" for serving remotely or "bytes" for serving locally
-export const diskImageType = "cloud";
+// original:
+// export const diskImageType = "cloud";
+export const diskImageType = "bytes";
 // Print an introduction message about the technology
 export const printIntro = true;
 // Is a graphical display needed
@@ -13,9 +17,9 @@ export const args = ["--login"];
 // Optional extra parameters
 export const opts = {
 	// Environment variables
-	env: ["HOME=/home/user", "TERM=xterm", "USER=user", "SHELL=/bin/bash", "EDITOR=vim", "LANG=en_US.UTF-8", "LC_ALL=C"],
+	env: ["HOME=/home/student", "TERM=xterm", "USER=student", "SHELL=/bin/bash", "EDITOR=nano", "LANG=en_US.UTF-8", "LC_ALL=C"],
 	// Current working directory
-	cwd: "/home/user",
+	cwd: "/home/student",
 	// User id
 	uid: 1000,
 	// Group id
